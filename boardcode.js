cc04f94ac2f6b7c04797eaec749548b02412f20e
wonderingabout <dota2orgoweiqibaduk@gmail.com>
2020-03-01 18:51:49 +0100

    move class independent functions to the bottom of the code

--- boardcode.js
+++ boardcode.js
@@ -1,5 +1,6 @@
 // go game server:
 
+"use strict";
 // make a class later
 
 const Game = {
@@ -10,13 +11,8 @@ const Game = {
               isTeam: false,
             },
     board: [],
-    createBoard() {
-        for (let i = 0; i <= 18; i++) {
-            this.board[i] = [];
-            for (let j = 0; j <= 18; j++) {
-                this.board[i][j] = null;
-            }
-        }
+    createNewBoard() {
+        this.board = getNewBoard();
     },
     line: null, // ex: 4
     colm: null, // ex: 3
@@ -24,22 +20,12 @@ const Game = {
     lines: [],
     colms: [],
     moves: [],
-    getLineColm(server) {
+    fetchLineColm(server) {
         this.line = server.line;
         this.colm = server.colm;
     },
-    checkLineColmIsNotValid(line, colm) {
-        return [line, colm].some( (e) => e < 0 || e > 19 );
-    },
-    convertLineColmToMove() {
-        if (this.line === "pass" && this.colm === "pass") {
-            this.move = "pass";
-        } else {
-            const letters = ["A", "B", "C", "D", "E", "F", "G",
-                             "H", "I", "J", "K", "L", "M", "N",
-                             "O", "P", "Q", "R", "S", "T"];
-            this.move = `${letters[this.line]}${this.colm + 1}`;
-        }
+    assignMove() {
+        this.move = convertLineColmToMove(this.line, this.colm);
     },
     checkRepeatedMoveTwoAgo() {
         // ...... repeated board position is more complex than just
@@ -48,106 +34,28 @@ const Game = {
         // start with this for now
         return (this.moves[this.moves.length - 2] === move);
     },
-    checkSnapback() {
-        //const fakeCapture = ......................
-        return ((this.captures.length - 1) !== fakeCapture);
-    },
-    getNewGroup(x, y, gameBoard) {
-        return { color: gameBoard[x][y],
-                 oppositeColor: this.getOppositeColor(gameBoard[x][y]),
-                 board: this.createBoard(),
-                 lines: [],
-                 colms: [],
-                 liberties: { lines: [],
-                              colms: []
-                            }
-               };
-    },
-    // using recursion:
-    // ex: of how it works provided by @Dorus: https://github.com/Dorus
-    // inspired on https://en.m.wikipedia.org/wiki/Flood_fill
-    /*  input (gameBoard):
-
-        ....
-        WBB.
-        .WB.
-        ..W.
-
-        output (groupBoard) for x = 2 and y = 2:
-
-        .LL.
-        .BBL
-        ..BL
-        ....
-
-    */
-    fill(x, y, gameBoard, group) {
-        if (this.checkLineColmIsNotValid(x, y)) return; // check if stone is out of board edges
-
-        if (gameBoard[x][y] === group.oppositeColor) return; // check if stone is not part of our group (opposite color)
-        if (!gameBoard[x][y]) {
-            group.board[x][y] = "L"; // liberty of the group (empty stone)
-            group.liberties.lines.push(x);
-            group.liberties.colms.push(y);
-            return;
-        }
-
-        // else gameBoard[x][y] === group.color, do we want to add it ?
-        if (group.board[x][y] === group.color || group.board[x][y] === "L") return; // check if we have done this spot before
-
-        group.board[x][y] = group.color; // mark group.board spot as done (our group color),
-        group.lines.push(x); // keep track of all our group stone coordinates
-        group.colms.push(y);
-        // then check the leaves of that marked stone of our group
-        fill(x    , y + 1, gameBoard, group);
-        fill(x    , y - 1, gameBoard, group);
-        fill(x + 1, y    , gameBoard, group);
-        fill(x - 1, y    , gameBoard, group);
-    },
-    getGroup(x, y, gameBoard) {
-        // check if there is no stone (hence no group) in that position
-        if (!gameBoard[x][y]) {
-            return;
-        }
-        const group = this.getNewGroup(x, y, gameBoard);
-
-        this.fill(x, y, gameBoard, group);
-        return group;
-    },
     assignGroup() {
-        this.group = this.getGroup(this.line, this.colm, this.board);
-    },
-    checkGroupIsCapturable(group) {
-        if (group.liberties.lines.length === 1 &&
-            group.liberties.lines[0] === this.line &&
-            group.liberties.colms[0] === this.colm) {
-            return true;
-        }
-        return false;
+        this.group = getGroup(this.line, this.colm, this.board);
     },
     captures: { B: [],
                 W: []
               },
-    removeGroup(group) {
-        // remove all stones of dead group, do not play our move
-        const groupLength = this.group.lines.length;
-        for (i in [...Array(groupLength)]) {
-            const lines = this.group.lines;
-            const colms = this.group.colms;
-            this.board[lines[i]][colms[i]] = null;
-        }
+    playCaptureGroup() {
+        removeGroup(this.group, this.board);
         this.captures[group.oppositeColor].push(groupLength);
         this.captures[group.color].push(0);
+        this.board[this.line][this.colm] = this.player.color;
     },
-    fakeCaptureGroup(group) {
-        // don't actually capture, return a copy of what
-        // the future board would be if the capture happened 
+    fakeCaptureGroup() {
+        const fakeBoard = { ...this.board };
+        removeGroup(this.group, fakeBoard);
+    },
+    checkSnapback() {
+        //const fakeCapture = ......................
+        return ((this.captures.length - 1) !== fakeCapture);
     },
     checkSuicide() {
         // return true if the move is played in the last liberty of the group
-    },
-    showBoardMsg(msg) {
-        // post msg through server API
     },
     score: null,
     winner: null,
@@ -155,7 +63,7 @@ const Game = {
         return (this.move === "pass");
     },
     checkDoublePass() {
-        return (this.move === this.moves[this.moves.length - 1]);
+        return (this.checkPass() && (this.move === this.moves[this.moves.length - 1]));
     },
     statuses: { isFull: false,
                 isPlaying: false,
@@ -163,16 +71,6 @@ const Game = {
                 isResigned: false,
                 isFinished: false
               },
-    checkBoardIsFull(board) {
-        for (let i = 0; i <= 18; i++) {
-            for (let j = 0; j <= 18; j++) {
-                if (!board[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    },
     updateStatus() {
         for (status in this.statuses) {
             // we should not need to update these if we enforce
@@ -196,17 +94,14 @@ const Game = {
         this.colms.push(this.colm);
     
     },
-    getOppositeColor(color) {
-        return (color === "B" ? "W" : "B");
-    },
     switchPlayer() {
-        this.player.color = getOppositePlayer();
+        this.player.color = getOppositePlayer(this.player.color);
     },
     playGame() {
         this.createBoard();
 
         while (!this.gameState.values.some( (bool) => Boolean(bool)) ) {
-            this.getMove(server);
+            this.fetchLineColm(server);
             if (!this.checkPass()) {
                 if (this.board[this.line][this.colm]) {
                     this.showBoardMsg(`Invalid board move requested: already filled`);
@@ -251,3 +146,135 @@ const Game = {
 };
 
 Game.playGame();
+
+function convertLineColmToMove(line, colm) {
+    if (line === "pass" && colm === "pass") {
+        return "pass";
+    } else {
+        const letters = ["A", "B", "C", "D", "E", "F", "G",
+                         "H", "I", "J", "K", "L", "M", "N",
+                         "O", "P", "Q", "R", "S", "T"];
+        return `${letters[line]}${colm + 1}`;
+    }
+}
+
+function checkLineColmIsNotValid(line, colm) {
+    return [line, colm].some( (e) => e < 0 || e > 19 );
+}
+
+function getNewBoard() {
+    const newBoard = [];
+    for (let i = 0; i <= 18; i++) {
+        newBoard[i] = [];
+        for (let j = 0; j <= 18; j++) {
+            newBoard[i][j] = null;
+        }
+    }
+    return newBoard;
+}
+
+function getNewGroup(x, y, gameBoard) {
+    return { color: gameBoard[x][y],
+             oppositeColor: getOppositeColor(gameBoard[x][y]),
+             board: getNewBoard(),
+             lines: [],
+             colms: [],
+             liberties: { lines: [],
+                          colms: []
+                        }
+           };
+}
+
+// using recursion:
+// example and initial code are provided by @Dorus: https://github.com/Dorus
+// inspired on https://en.m.wikipedia.org/wiki/Flood_fill
+/*  input (gameBoard):
+
+    ....
+    WBB.
+    .WB.
+    ..W.
+
+    output (groupBoard) for x = 2 and y = 2:
+
+    .LL.
+    .BBL
+    ..BL
+    ....
+
+*/
+function fill(x, y, gameBoard, group) {
+    if (checkLineColmIsNotValid(x, y)) return; // check if stone is out of board edges
+
+    if (gameBoard[x][y] === group.oppositeColor) return; // check if stone is not part of our group (opposite color)
+    if (!gameBoard[x][y]) {
+        group.board[x][y] = "L"; // liberty of the group (empty stone)
+        group.liberties.lines.push(x);
+        group.liberties.colms.push(y);
+        return;
+    }
+
+    // else gameBoard[x][y] === group.color, do we want to add it ?
+    if (group.board[x][y] === group.color || group.board[x][y] === "L") return; // check if we have done this spot before
+
+    group.board[x][y] = group.color; // mark group.board spot as done (our group color),
+    group.lines.push(x); // keep track of all our group stone coordinates
+    group.colms.push(y);
+    // then check the leaves of that marked stone of our group
+    fill(x    , y + 1, gameBoard, group);
+    fill(x    , y - 1, gameBoard, group);
+    fill(x + 1, y    , gameBoard, group);
+    fill(x - 1, y    , gameBoard, group);
+}
+
+function getGroup(x, y, gameBoard) {
+    // check if there is no stone (hence no group) in that position
+    if (!gameBoard[x][y]) {
+        return;
+    }
+    const group = this.getNewGroup(x, y, gameBoard);
+
+    fill(x, y, gameBoard, group);
+    delete group.board; // we don't need it anymore, we can recreate it anytime if needed with group coordinates
+    return group;
+}
+
+function checkGroupIsCapturable(group, line, colm) {
+    if (group.liberties.lines.length === 1 &&
+        group.liberties.lines[0] === line &&
+        group.liberties.colms[0] === colm) {
+        return true;
+    }
+    return false;
+}
+
+function removeGroup(group, board) {
+    // return the passed board with all stones of dead group removed, 
+    // do not play our move
+    for (i in [...Array(group.lines.length)]) {
+        const lines = group.lines;
+        const colms = group.colms;
+        board[lines[i]][colms[i]] = null;
+    }
+}
+
+// TODO remaining:
+
+function checkBoardIsFull(board) {
+    for (let i = 0; i <= 18; i++) {
+        for (let j = 0; j <= 18; j++) {
+            if (!board[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function getOppositeColor(color) {
+    return (color === "B" ? "W" : "B");
+}
+
+function showBoardMsg(msg, server) {
+    // post msg through server API
+}
